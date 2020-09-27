8160a68e0d9031ab42605cf8d9316179b758e9f5
sabrey <trinityforce37@gmail.com>
2020-09-28 00:13:39 +0300

    1 video scrapper added.
    >get1 https://www.youtube.com/watch?v=fzI9FNjXQ0o
    
    video will download current location.

---
+++ get1video.py
@@ -0,0 +1,25 @@
+# getemall_yt
+# this program is made for downloading all videos in a channel.
+# you can change video format and quality
+# this program require  ffmpeg , youtube-dl and chrome driver. download  these and put in windows file or add as  enviroment path
+# https://chromedriver.chromium.org
+# https://youtube-dl.org
+# https://ffmpeg.org
+# creator: sabrey
+import sys
+from selenium import webdriver
+import os
+
+driver = webdriver.Chrome()
+
+
+def main():
+    print("Youtube Video Scrapper  by Sabrey \n")
+    url = sys.argv[1]
+    command = "youtube-dl " + url
+    print("downloading...")
+    os.system(command)
+
+
+if __name__ == "__main__":
+    main()

5022f48e3e3ae87355c5f36706c052097d959f9c
sabrey <trinityforce37@gmail.com>
2020-10-20 15:08:22 +0300

    images added

--- getem.c
+++ getem.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(int argc, char *argv[]){
-	execlp("python", "python", "getemall.py", argv[1], (char*) NULL);
+	execlp("python", "python", "D:/sabreys_cmd/getemall.py", argv[1], (char*) NULL);
 	return 0;
 }
 
